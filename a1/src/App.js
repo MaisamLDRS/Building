3dee5ef7363c1b24da3275b8a411a87049028e49
Koorikdat <maisamanjum@gmail.com>
2023-01-23 11:01:34 -0500

    first commit, has functioning responsive dropdown + footer

--- a1/src/App.js
+++ a1/src/App.js
@@ -2,19 +2,18 @@ import './App.css';
 import React from 'react';
 import Navbar from "./components/Navbar";
 import SocialFollow from "./components/SocialFollow"
+import { Widget } from 'react-chat-widget';
+
+
 
 function App() {
 	return (
-		<React.Fragment>
+	<React.Fragment>
 
-			<Navbar/>
-		
+		<Navbar/>
+      	<SocialFollow/>
 
-      <SocialFollow/>
-
-    </React.Fragment>
-
-    
+    </React.Fragment>    
 	);
 }
 
